4b72a692b03b1eb676343b125e2cb083e1709c83
absak <ante.bruno.sakic@fpmoz.sum.ba>
2024-07-06 13:57:52 +0200

    dodavanje-usera

---
+++ src/main/java/com/example/trelloapp/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.trelloapp.repositories;
+
+import com.example.trelloapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
